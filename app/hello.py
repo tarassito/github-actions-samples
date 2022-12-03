c6485678ded9dbac5d8453f618293cf08d8d2548
tarassito <tarassito@gmail.com>
2022-12-03 13:48:48 +0200

    Update hello.py

--- app/hello.py
+++ app/hello.py
@@ -7,7 +7,7 @@ config.read('config.properties')
 app = Flask(__name__)
 
 if config.getboolean("features", "feature_1") == True:
-	message = "Hello, Taras!!!"
+	message = "Hello, Taras!!"
 else:
 	message = "Hello, World!!!"
 
